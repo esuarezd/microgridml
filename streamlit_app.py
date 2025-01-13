e54893f9f84b3f2f24b07c2f7eccf10343847ca4
Edison Suarez <e.suarezd@uniandes.edu.co>
2025-01-13 16:37:42 -0500

    creacion streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Configuración de la página para usar todo el ancho disponible
+st.set_page_config(
+    layout="wide",  
+    page_title="microgrid ml",
+    page_icon="☀️"  # Ícono de sol
+)
+
+st.title("Aplicación Streamlit en Raspberry Pi")
+st.write("Hola, esta es mi primera aplicación web con Streamlit en la Raspberry Pi.")
